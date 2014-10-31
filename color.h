952f7516739e887ee06b5083b9d57494f24617df
Phoenix <Phoenix500526@163.com>
2014-11-01 00:50:00 +0800

    Create color.h
    This is the headfile of the 2048 game.It define some character string to control the color of output

---
+++ color.h
@@ -0,0 +1,17 @@
+#ifndef __COLOR__H
+#define __COLOR__H
+//定义字体颜色
+#define NONE "\033[m" //取消打印的颜色，打印完数字后就要取消掉颜色打印
+#define RED_2 "\033[0;40;31m"					//黑底红色	2
+#define GREEN_4 "\033[0;40;32m"					//黑底绿色	4
+#define DARKYELLOW_8 "\033[0;40;33m"			//黑底土黄	8	
+#define BLUE_16 "\033[0;40;36m"					//黑底蓝色	16	
+#define PURPLE_32 "\033[0;40;35m"				//黑底紫红	32
+#define LIGHT_ORANGE_64 "\033[1;40;31m"			//黑底浅橘  64
+#define ORANGE_128 "\033[1;40;32m"				//黑底橘红	128
+#define YELLOW_256 "\033[1;40;33m"				//黑底黄色	256
+#define LIGHT_PURPLE_512 "\033[1;40;34m"		//黑底浅紫	512
+#define PINK_1024 "\033[1;40;35m"				//黑底粉字	1024
+#define CAYN_2048 "\033[1;40;36m"				//黑底青色	2048
+#define BACKGROUND "\033[1;40m"					//黑色背景
+#endif //__COLOR__H
